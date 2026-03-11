9bfd4017022e9300b2983e4a6e1548330c9d8125
Nishant Ranjan <nishant.ranjan.air1@gmail.com>
2026-03-12 01:28:33 +0530

    UC1: Application entry point with welcome message

--- src/HotelBookingApp.java
+++ src/HotelBookingApp.java
@@ -35,6 +35,7 @@ public class HotelBookingApp {
     public static void main(String[] args) {
 
         System.out.println("=================================");
+        System.out.println("============Staylio===============");
         System.out.println("  Hotel Booking Management System");
         System.out.println("  Version 1.0");
         System.out.println("=================================");
